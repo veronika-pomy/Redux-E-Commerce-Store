c03c51d67ded711dc8dea9abdf1095a1d588cee2
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-05-07 17:04:50 -0400

    added readme template, added redux comments to front-end

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -21,7 +21,7 @@ const InitalState = {
   };
 
 // The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
-export const reducer = (state = InitalState, action) => {
+export default function reducer (state = InitalState, action) {
   switch (action.type) {
     // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
     case UPDATE_PRODUCTS:
